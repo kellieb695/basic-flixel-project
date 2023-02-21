8479dbcfd77755e9165824550459fc3d39ee7021
Kellie Behm <kb695@live.missouristate.edu>
2023-02-21 12:35:05 -0600

    Add hero class and hero to playstate

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,15 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var hero:Hero;
 	override public function create()
-	{
+	{	
+		hero = new Hero(300,300);
+		add(hero);
 		super.create();
 	}
 
