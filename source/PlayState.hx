f9f9c385406cf8bec0bf385f5e38fefdc52f9f90
Kellie Behm <kb695@live.missouristate.edu>
2023-02-21 21:04:30 -0600

    Hello world text object

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,15 +2,24 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
+
 	var hero:Hero;
 	override public function create()
 	{	
 		hero = new Hero(300,300);
 		add(hero);
+
+		var text = new FlxText(260,220,200, "hellooo world!!",20);
+		text.font = "Book Antiqua";
+		add(text); 
 		super.create();
+
+
+
 	}
 
 	override public function update(elapsed:Float)
