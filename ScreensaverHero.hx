30f1d3d457a9358980b5d651a04dacb6655e5269
Kellie Behm <kb695@live.missouristate.edu>
2023-02-23 10:25:12 -0600

    hero movement wraps around screen

--- ScreensaverHero.hx
+++ ScreensaverHero.hx
@@ -2,7 +2,6 @@ package;
 
     import flixel.FlxSprite;
     import flixel.util.FlxColor;
-    import flixel.math.FlxRandom;
     import flixel.FlxG;
 
     class ScreensaverHero extends FlxSprite {
@@ -10,33 +9,38 @@ package;
         public function new(x:Int = 0, y:Int = 0){
             super(x,y);
             super.makeGraphic(30, 40);
+        
         }
 
         var hue:Float = 0;
-        var velocityX:Float;
-        var velocityY:Float;
         
 
         override public function update(elapsed:Float){
             
-            velocityX = 20;
-            velocityY = 3;
+            
             hue += this.x;
-            this.velocity.x = velocityX;
-            this.velocity.y = velocityY;
+            
             if (hue > 360)
                     hue -= 360;
             var color = FlxColor.fromHSB(Std.int(hue), 1, 1);
             this.color = color;
 
-            if (this.velocity.x > 0 && this.x >= FlxG.width - this.width){
-                this.x = FlxG.width - this.width;
-                this.velocity.x *= -1;
+            if (this.x >= FlxG.width - (this.width/2)){
+                this.x = 0;
             }
             else if (this.velocity.x < 0 && this.x <= 0){
                 this.x = 0;
                 this.velocity.x *= -1;
             }
+
+            if (this.y >= FlxG.height - (this.height/2)){
+                this.y = 0;
+            }
+            else if (this.velocity.y < 0 && this.y <= 0){
+                this.y = 0;
+                this.velocity.y *= -1;
+            }
+            
 
 
             super.update(elapsed);
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.math.FlxRandom;
 
 class PlayState extends FlxState
 {
@@ -19,6 +20,12 @@ class PlayState extends FlxState
 		screensaverHero1 = new ScreensaverHero(50,50);
 		screensaverHero2 = new ScreensaverHero(100,100);
 		screensaverHero3 = new ScreensaverHero(400,400);
+		screensaverHero1.velocity.x = 70;
+		screensaverHero1.velocity.y = 50;
+		screensaverHero2.velocity.x = 50;
+		screensaverHero2.velocity.y = 40;
+		screensaverHero3.velocity.x = 50;
+		screensaverHero3.velocity.y = 70;
 		add(screensaverHero1);
 		add(screensaverHero2);
 		add(screensaverHero3);
